3cf95ff879944039240a457f7e330c83db208908
andkom <andkmrv@gmail.com>
2021-11-30 13:28:12 +0200

    remove near from aggregator config

--- libs/config/src/near-config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('near', () => ({
-  env: process.env.NEAR_ENV || 'development',
-  credentials: process.env.NEAR_CREDENTIALS_DIR || '.near-credentials',
-}));
