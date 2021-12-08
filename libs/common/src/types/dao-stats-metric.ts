d133480315cc126caed7e718f283816a170ec693
Oleg Kalenik <okalenyk@gmail.com>
2021-12-08 12:20:23 +0200

    *added dao count metric aggregation

--- libs/common/src/types/dao-stats-metric.ts
+++ libs/common/src/types/dao-stats-metric.ts
@@ -1,6 +1,7 @@
 export enum DaoStatsMetric {
   GroupsCount = 'GROUPS_COUNT',
   CouncilSize = 'COUNCIL_SIZE',
+  DaoCount = 'DAO_COUNT',
   ProposalsCount = 'PROPOSALS_COUNT',
   ProposalsPayoutCount = 'PROPOSALS_PAYOUT_COUNT',
   ProposalsCouncilMemberCount = 'PROPOSALS_COUNCIL_MEMBER_COUNT',
