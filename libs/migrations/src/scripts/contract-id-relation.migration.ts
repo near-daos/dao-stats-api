4f1d4091a17267f7217e5885ada0334308bdeb71
Oleg Kalenik <okalenyk@gmail.com>
2021-12-09 14:40:25 +0200

    *added migrations for missing receipt/receipt_action 'contractId' relations

---
+++ libs/migrations/src/scripts/contract-id-relation.migration.ts
@@ -0,0 +1,47 @@
+import { Transaction } from '@dao-stats/common/entities';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import PromisePool from '@supercharge/promise-pool';
+import { Migration } from '..';
+
+@Injectable()
+export class ContractIdRelationMigration implements Migration {
+  private readonly logger = new Logger(ContractIdRelationMigration.name);
+
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionRepository: Repository<Transaction>,
+  ) {}
+
+  public async migrate(): Promise<void> {
+    this.logger.log('Starting Contract ID migration...');
+
+    const transactions = await this.transactionRepository
+      .createQueryBuilder('transaction')
+      .leftJoinAndSelect('transaction.receipts', 'receipts')
+      .leftJoinAndSelect('receipts.receiptActions', 'action_receipt_actions')
+      .getMany();
+
+    await PromisePool.withConcurrency(500)
+      .for(transactions)
+      .handleError((error) => {
+        this.logger.error(error);
+      })
+      .process(async (tx) => {
+        await this.transactionRepository.save({
+          ...tx,
+          receipts: tx.receipts.map((receipt) => ({
+            ...receipt,
+            contractId: tx.contractId,
+            receiptActions: receipt.receiptActions.map((receiptAction) => ({
+              ...receiptAction,
+              contractId: tx.contractId,
+            })),
+          })),
+        });
+      });
+
+    this.logger.log('Finished Contract ID migration.');
+  }
+}
