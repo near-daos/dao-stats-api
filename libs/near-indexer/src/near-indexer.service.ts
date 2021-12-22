c8406ac794f5c9bf156c75ff9c8a1e339285b732
andkom <andkmrv@gmail.com>
2021-12-22 18:17:29 +0200

    near indexer fixes

--- libs/near-indexer/src/near-indexer.service.ts
+++ libs/near-indexer/src/near-indexer.service.ts
@@ -1,6 +1,6 @@
+import { Connection, SelectQueryBuilder } from 'typeorm';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Connection, Repository, SelectQueryBuilder } from 'typeorm';
 import { NEAR_INDEXER_DB_CONNECTION } from './constants';
 import { Transaction } from './entities';
 
@@ -17,11 +17,7 @@ export class NearIndexerService {
     accountIds: string | string[],
     fromBlockTimestamp?: bigint,
   ): Promise<Transaction> {
-    return this.buildAggregationTransactionQuery(
-      this.connection.getRepository(Transaction),
-      accountIds,
-      fromBlockTimestamp,
-    )
+    return this.buildAggregationTransactionQuery(accountIds, fromBlockTimestamp)
       .select('transaction.transactionHash')
       .orderBy('transaction.block_timestamp', 'ASC')
       .getOne();
@@ -34,7 +30,6 @@ export class NearIndexerService {
     toBlockTimestamp?: bigint,
   ): Promise<Transaction[]> {
     return this.buildAggregationTransactionQuery(
-      this.connection.getRepository(Transaction),
       accountIds,
       fromBlockTimestamp,
       toBlockTimestamp,
@@ -48,36 +43,37 @@ export class NearIndexerService {
   }
 
   private buildAggregationTransactionQuery(
-    repository: Repository<Transaction>,
     accountIds: string | string[],
     fromBlockTimestamp?: bigint,
     toBlockTimestamp?: bigint,
   ): SelectQueryBuilder<Transaction> {
-    let queryBuilder = repository
+    const queryBuilder = this.connection
+      .getRepository(Transaction)
       .createQueryBuilder('transaction')
       .leftJoinAndSelect('transaction.receipts', 'receipts')
       .leftJoinAndSelect('receipts.receiptActions', 'action_receipt_actions');
 
-    queryBuilder =
-      accountIds instanceof Array
-        ? queryBuilder.where('transaction.receiver_account_id IN (:...ids)', {
-            ids: accountIds,
-          })
-        : queryBuilder.where('transaction.receiver_account_id LIKE :id', {
-            id: `%${accountIds}`,
-          });
+    if (Array.isArray(accountIds)) {
+      queryBuilder.where('transaction.receiver_account_id IN (:...ids)', {
+        ids: accountIds,
+      });
+    } else {
+      queryBuilder.where('transaction.receiver_account_id LIKE :id', {
+        id: `%${accountIds}`,
+      });
+    }
 
-    queryBuilder = fromBlockTimestamp
-      ? queryBuilder.andWhere('transaction.block_timestamp >= :from', {
-          from: String(fromBlockTimestamp),
-        })
-      : queryBuilder;
+    if (fromBlockTimestamp) {
+      queryBuilder.andWhere('transaction.block_timestamp >= :from', {
+        from: String(fromBlockTimestamp),
+      });
+    }
 
-    queryBuilder = toBlockTimestamp
-      ? queryBuilder.andWhere('transaction.block_timestamp <= :to', {
-          to: String(toBlockTimestamp),
-        })
-      : queryBuilder;
+    if (toBlockTimestamp) {
+      queryBuilder.andWhere('transaction.block_timestamp <= :to', {
+        to: String(toBlockTimestamp),
+      });
+    }
 
     return queryBuilder;
   }
