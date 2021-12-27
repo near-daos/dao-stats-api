498caaf21bdc3c421d03327c8b9cd35305e1eaac
andkom <andkmrv@gmail.com>
2021-12-27 20:55:08 +0200

    use config service

--- apps/api/src/interceptors/http-cache.interceptor.ts
+++ apps/api/src/interceptors/http-cache.interceptor.ts
@@ -1,12 +1,25 @@
 import {
   CACHE_KEY_METADATA,
+  CACHE_MANAGER,
   CacheInterceptor,
   ExecutionContext,
+  Inject,
   Injectable,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+const REFLECTOR = 'Reflector';
 
 @Injectable()
 export class HttpCacheInterceptor extends CacheInterceptor {
+  constructor(
+    @Inject(CACHE_MANAGER) protected readonly cacheManager: any,
+    @Inject(REFLECTOR) protected readonly reflector: any,
+    private readonly configService: ConfigService,
+  ) {
+    super(cacheManager, reflector);
+  }
+
   trackBy(context: ExecutionContext): string | undefined {
     const cacheKey = this.reflector.get(
       CACHE_KEY_METADATA,
@@ -22,6 +35,8 @@ export class HttpCacheInterceptor extends CacheInterceptor {
   }
 
   isRequestCacheable(): boolean {
-    return !['dev', 'development', 'local'].includes(process.env.NODE_ENV);
+    return !['dev', 'development', 'local'].includes(
+      this.configService.get('environment'),
+    );
   }
 }
