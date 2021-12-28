95096678395be9bd2dc00f5ab83a252b8f70c672
andkom <andkmrv@gmail.com>
2021-12-28 20:10:05 +0200

    add new metric groups

--- libs/common/src/types/dao-stats-metric-group.ts
+++ libs/common/src/types/dao-stats-metric-group.ts
@@ -1,6 +1,19 @@
 import { DaoStatsMetric } from './dao-stats-metric';
 
 export const DaoStatsMetricGroup = {
+  ActionsCount: [DaoStatsMetric.ActionsInCount, DaoStatsMetric.ActionsOutCount],
+  ActionsActionsFunctionCallCount: [
+    DaoStatsMetric.ActionsFunctionCallInCount,
+    DaoStatsMetric.ActionsFunctionCallOutCount,
+  ],
+  ActionsDepositCount: [
+    DaoStatsMetric.ActionsDepositInCount,
+    DaoStatsMetric.ActionsDepositOutCount,
+  ],
+  ActionsDepositValue: [
+    DaoStatsMetric.ActionsDepositInValue,
+    DaoStatsMetric.ActionsDepositOutValue,
+  ],
   BountiesAndGrantsValueLocked: [DaoStatsMetric.BountiesValueLocked],
   TotalValueLocked: [
     DaoStatsMetric.BountiesValueLocked,
